13ced190ef54da7439d5ac1189d9fac1578c892e
Aaron Tripp <113567536+Aaron-Trippy@users.noreply.github.com>
2023-09-13 14:00:20 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hey!</h1>
+      <h2>Let's do the thing and make a site.</h2>
+    </div>
+  );
+}
